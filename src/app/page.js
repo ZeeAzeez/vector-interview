43a4bab42b5972494ac8fb4935c021399c0be9c9
ZeeAzeez <azeezahaazeez@gmail.com>
2025-03-19 12:14:20 +0100

    fix: Set Candidate Interview Page as default page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/dashboard"); // Automatically redirect to Dashboard
+  redirect("/candidate-interview-page"); // Automatically redirect to Dashboard
   return null; // Nothing is rendered
 }
